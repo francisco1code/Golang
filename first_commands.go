2cdceb2644a3791f073c7b1fbeacfbab3e30b1a9
francisco1code <francisco170142329@gmail.com>
2020-12-10 01:27:29 -0300

    first commands

---
+++ first_commands.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt" 
+
+func main()  {
+
+	fmt.Println("First commands")
+
+	var name string	= "Francisco"
+
+	fmt.Println(name);
+	
+}
+
+// to run the ".go" file: go run "name file"
